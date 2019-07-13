ece68b4354df07c2852c860416adb51db386543e
Dylam De La Torre <dyxel04@gmail.com>
2019-07-13 23:44:24 +0200

    update drawing api types to be more cleare

--- src/client/drawing/types.hpp
+++ src/client/drawing/types.hpp
@@ -11,18 +11,26 @@ namespace Drawing
 // Position
 using Vertex = glm::vec3;
 using Vertices = std::vector<Vertex>;
-inline extern const std::size_t VERTEX_SIZE = sizeof(Vertex::value_type) *
-                                              Vertex::length();
+constexpr std::size_t VERTEX_LENGTH = 3;
+constexpr std::size_t VERTEX_SIZE = sizeof(Vertex);
+
 // Color
 using Color = glm::vec4;
 using Colors = std::vector<Color>;
-inline extern const std::size_t COLOR_SIZE = sizeof(Color::value_type) *
-                                      Color::length();
+constexpr std::size_t COLOR_LENGTH = 4;
+constexpr std::size_t COLOR_SIZE = sizeof(Color);
+
+// Index
+using Index = glm::vec2;
+using Indeces = std::vector<Index>;
+constexpr std::size_t INDEX_LENGTH = 2;
+constexpr std::size_t INDEX_SIZE = sizeof(Index);
+
 // Texture Coordinate
 using TexCoord = glm::vec2;
 using TexCoords = std::vector<TexCoord>;
-inline extern const std::size_t TEXCOORD_SIZE = sizeof(TexCoord::value_type) *
-                                                TexCoord::length();
+constexpr std::size_t TEXCOORD_LENGTH = 2;
+constexpr std::size_t TEXCOORD_SIZE = sizeof(TexCoord);
 
 } // Drawing
 #endif // DRAWING_TYPES_HPP
