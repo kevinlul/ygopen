1915c6be25bd85ac7cdcf50e94d5662c8cf8bf1c
Dylam De La Torre <dyxel04@gmail.com>
2019-07-15 09:29:34 +0200

    add missing definition

--- src/client/drawing/types.hpp
+++ src/client/drawing/types.hpp
@@ -32,5 +32,17 @@ using TexCoords = std::vector<TexCoord>;
 constexpr std::size_t TEXCOORD_LENGTH = 2;
 constexpr std::size_t TEXCOORD_SIZE = sizeof(TexCoord);
 
+// Primitive drawing modes
+enum PrimitiveDrawMode
+{
+	PDM_POINTS = 0, // Separated points
+	PDM_LINE_STRIP = 1, // Lines, current one connected to previous one
+	PDM_LINE_LOOP = 2, // Lines, same as above, last connected to first
+	PDM_LINES = 3, // Lines, each one taking a span of 2 vertices
+	PDM_TRIANGLE_STRIP = 4, // Triangles, last two vertices connected to current
+	PDM_TRIANGLE_FAN = 5, // Triangles, current connected to first and last
+	PDM_TRIANGLES = 6 // Triangles, each one taking a span of 3 vertices
+};
+
 } // Drawing
 #endif // DRAWING_TYPES_HPP
